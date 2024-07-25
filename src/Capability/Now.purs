4c30feaeb5d837afd6c17450d448d91e5ca9a6fa
igor-primo <igorprimo62@gmail.com>
2024-07-25 10:07:01 -0300

    feat: adiciona capability de data
    E também adiciona funcionalidade de direnv ao projeto.

---
+++ src/Capability/Now.purs
@@ -0,0 +1,21 @@
+module Loja.Capability.Now where
+
+import Prelude
+
+import Control.Monad.Trans.Class (lift)
+import Data.DateTime (Date, DateTime, Time)
+import Data.DateTime.Instant (Instant)
+import Halogen (HalogenM)
+
+-- Declaração da capability de construir dados de data.
+class Monad m <= Now m where
+  now :: m Instant
+  nowDate :: m Date
+  nowTime :: m Time
+  nowDateTime :: m DateTime
+
+instance nowHalogenM :: Now m => Now (HalogenM st act slots msg m) where
+  now = lift now
+  nowDate = lift nowDate
+  nowTime = lift nowTime
+  nowDateTime = lift nowDateTime
